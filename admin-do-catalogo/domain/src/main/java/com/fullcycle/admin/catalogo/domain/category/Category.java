a0114d9baa373cd62c7fefa9d3ea9367943c71b9
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-18 22:07:58 -0300

    feat(domain): finish implementation of category entity

--- admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -1,10 +1,12 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
-import java.time.Instant;
-import java.util.UUID;
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
+import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
 
-public class Category {
-    private String id;
+import java.time.Instant;
+import java.util.Objects;
+
+public class Category extends AggregateRoot<CategoryId> {
     private String name;
     private String description;
     private boolean active;
@@ -13,29 +15,69 @@ public class Category {
     private Instant deletedAt;
 
 
-    private Category(final String id,
-                    final String name,
-                    final String description,
-                    final boolean active,
-                    final Instant createdAt,
-                    final Instant updatedAt,
-                    final Instant deletedAt
+    private Category(final CategoryId anId,
+                    final String aName,
+                    final String aDescription,
+                    final boolean isActive,
+                    final Instant aCreationDate,
+                    final Instant aUpdateDate,
+                    final Instant aDeleteDate
     ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = Objects.requireNonNull(aCreationDate, "'createdAt' should not be null");
+        this.updatedAt = Objects.requireNonNull(aUpdateDate, "'updatedAt' should not be null");
+        this.deletedAt = aDeleteDate;
     }
 
     public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
-        final var id = UUID.randomUUID().toString();
-        return new Category(id, aName, aDescription, isActive, Instant.now(), Instant.now(), null);
+        final var id = CategoryId.unique();
+        final var now = Instant.now();
+        final var deletedAt = isActive ? null : now;
+        return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
-    public String getId() {
+    @Override
+    public void validate(ValidationHandler handler) {
+        new CategoryValidator(this, handler).validate();
+    }
+
+    public Category activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = Instant.now();
+        }
+
+        this.active = false;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category update(
+            final String aName,
+            final String aDescription,
+            final boolean isActive
+    ) {
+        if (isActive) {
+            activate();
+        } else {
+            deactivate();
+        }
+        this.name = aName;
+        this.description = aDescription;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public CategoryId getId() {
         return id;
     }
 
---
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,22 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    void deleteById(CategoryId anId);
+
+    Optional<Category> findById(CategoryId anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(SearchQuery aQuery);
+
+    List<CategoryId> existsByIds(Iterable<CategoryId> ids);
+}
