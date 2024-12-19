439ce3b7c8c2ec74b8939e290aee36fe6a52b4ea
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-19 14:49:44 -0300

    feat(infrastructure): create category gateway implementation

--- admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/CategoryId.java
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/CategoryId.java
@@ -1,44 +1,40 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
 import com.fullcycle.admin.catalogo.domain.Identifier;
+import com.fullcycle.admin.catalogo.domain.utils.IdUtils;
 
 import java.util.Objects;
-import java.util.UUID;
 
 public class CategoryId extends Identifier {
     private final String value;
 
     private CategoryId(final String value) {
-        Objects.requireNonNull(value, "'value' must not be null");
-        this.value = value;
+        this.value = Objects.requireNonNull(value);
     }
 
     public static CategoryId unique() {
-        return CategoryId.from(UUID.randomUUID());
+        return CategoryId.from(IdUtils.uuid());
     }
 
     public static CategoryId from(final String anId) {
         return new CategoryId(anId);
     }
 
-    public static CategoryId from(final UUID anId) {
-        return new CategoryId(anId.toString().toLowerCase());
-    }
-
+    @Override
     public String getValue() {
         return value;
     }
 
     @Override
-    public boolean equals(final Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final CategoryId that = (CategoryId) o;
-        return Objects.equals(getValue(), that.getValue());
+        return getValue().equals(that.getValue());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(getValue());
+        return Objects.hash(getValue());
     }
 }
