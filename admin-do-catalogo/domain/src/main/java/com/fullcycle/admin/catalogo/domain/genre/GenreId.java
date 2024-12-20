17177d4f794c64034c456e0b9c1b0372d3160fd6
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-20 15:01:43 -0300

    feat(domain): create genre aggregate root

---
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/genre/GenreId.java
@@ -0,0 +1,42 @@
+package com.fullcycle.admin.catalogo.domain.genre;
+
+import com.fullcycle.admin.catalogo.domain.Identifier;
+import com.fullcycle.admin.catalogo.domain.utils.IdUtils;
+
+import java.util.Objects;
+
+public class GenreId extends Identifier {
+
+    private final String value;
+
+    private GenreId(final String value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    public static GenreId unique() {
+        return GenreId.from(IdUtils.uuid());
+    }
+
+    public static GenreId from(final String anId) {
+        return new GenreId(anId);
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final GenreId that = (GenreId) o;
+        return getValue().equals(that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+}
