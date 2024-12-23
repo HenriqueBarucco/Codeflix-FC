d63431460452f32e3a24f911934d8d4baae97ac2
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-23 14:35:18 -0300

    feat(domain): create cast member domain

---
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberGateway.java
@@ -0,0 +1,22 @@
+package com.fullcycle.admin.catalogo.domain.castmember;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CastMemberGateway {
+
+    CastMember create(CastMember aCastMember);
+
+    void deleteById(CastMemberId anId);
+
+    Optional<CastMember> findById(CastMemberId anId);
+
+    CastMember update(CastMember aCastMember);
+
+    Pagination<CastMember> findAll(SearchQuery aQuery);
+
+    List<CastMemberId> existsByIds(Iterable<CastMemberId> ids);
+}
