431a07888d5da656114d906b70c6aae040d70498
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-26 19:12:57 -0300

    feat(application): create video usecases

---
+++ admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/MediaResourceGateway.java
@@ -0,0 +1,14 @@
+package com.fullcycle.admin.catalogo.domain.video;
+
+import java.util.Optional;
+
+public interface MediaResourceGateway {
+
+    AudioVideoMedia storeAudioVideo(VideoId anId, VideoResource aResource);
+
+    ImageMedia storeImage(VideoId anId, VideoResource aResource);
+
+    Optional<Resource> getResource(VideoId anId, VideoMediaType type);
+
+    void clearResources(VideoId anId);
+}
