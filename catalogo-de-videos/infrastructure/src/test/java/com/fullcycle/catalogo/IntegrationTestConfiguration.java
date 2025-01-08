dbb905d237e8eec4b7a25815814ebc86fd5b5351
Henrique Barucco <henrique_barucco@hotmail.com>
2025-01-08 20:38:34 -0300

    feat(infrastructure): add elasticsearch configuration

---
+++ catalogo-de-videos/infrastructure/src/test/java/com/fullcycle/catalogo/IntegrationTestConfiguration.java
@@ -0,0 +1,18 @@
+package com.fullcycle.catalogo;
+
+import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+
+public class IntegrationTestConfiguration {
+
+    @Bean
+    public CategoryRepository categoryRepository() {
+        return Mockito.mock(CategoryRepository.class);
+    }
+
+    @Bean
+    public WebGraphQlSecurityInterceptor webGraphQlSecurityInterceptor() {
+        return new WebGraphQlSecurityInterceptor();
+    }
+}
