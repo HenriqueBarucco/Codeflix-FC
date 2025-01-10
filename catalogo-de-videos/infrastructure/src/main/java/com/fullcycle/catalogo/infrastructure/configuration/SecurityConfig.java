a3298a0fbc4b6869c06325f21175d60c14767348
Henrique Barucco <henrique_barucco@hotmail.com>
2025-01-10 20:02:51 -0300

    feat(infrastructure): create category kafka listener

--- catalogo-de-videos/infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/SecurityConfig.java
+++ catalogo-de-videos/infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.fullcycle.catalogo.infrastructure.configuration;
 
-import com.nimbusds.jose.shaded.gson.JsonObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,7 +8,6 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,39 +21,33 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @Profile("!development")
 public class SecurityConfig {
 
     private static final String ROLE_ADMIN = "CATALOGO_ADMIN";
-    private static final String ROLE_CAST_MEMBERS = "CATALOGO_CAST_MEMBERS";
-    private static final String ROLE_CATEGORIES = "CATALOGO_CATEGORIES";
-    private static final String ROLE_GENRES = "CATALOGO_GENRES";
-    private static final String ROLE_VIDEOS = "CATALOGO_VIDEOS";
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> {
+                    csrf.disable();
+                })
                 .authorizeHttpRequests(authorize -> {
                     authorize
-                            .requestMatchers("/cast_members*").hasAnyRole(ROLE_ADMIN, ROLE_CAST_MEMBERS)
-                            .requestMatchers("/categories*").hasAnyRole(ROLE_ADMIN, ROLE_CATEGORIES)
-                            .requestMatchers("/genres*").hasAnyRole(ROLE_ADMIN, ROLE_GENRES)
-                            .requestMatchers("/videos*").hasAnyRole(ROLE_ADMIN, ROLE_VIDEOS)
+                            .requestMatchers("/graphql", "/graphiql").permitAll()
                             .anyRequest().hasRole(ROLE_ADMIN);
                 })
                 .oauth2ResourceServer(oauth -> {
-                    oauth.jwt()
-                            .jwtAuthenticationConverter(new KeycloakJwtConverter());
+                    oauth.jwt(j -> j.jwtAuthenticationConverter(new KeycloakJwtConverter()));
                 })
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .headers(headers -> {
-                    headers.frameOptions().sameOrigin();
+                    headers.frameOptions(opt -> opt.sameOrigin());
                 })
                 .build();
     }
@@ -105,7 +97,7 @@ public class SecurityConfig {
             final Function<Map.Entry<String, Object>, Stream<String>> mapResource =
                     resource -> {
                         final var key = resource.getKey();
-                        final var value = (JsonObject) resource.getValue();
+                        final var value = (Map) resource.getValue();
                         final var roles = (Collection<String>) value.get(ROLES);
                         return roles.stream().map(role -> key.concat(SEPARATOR).concat(role));
                     };
